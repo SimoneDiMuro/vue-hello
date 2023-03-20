b1626925d3d8a1ca1244dbed7d521ffdb7c39dca
SimoneDiMuro <simone.dimuro@gmail.com>
2023-03-20 01:06:32 +0100

    Exercise finished

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const header = `<h1> Mi sto complicando la vita hehe </h1>`;
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      titolo: header,
+      imageSrc:
+        "https://images.unsplash.com/photo-1569081156361-c98e05e7f1e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+    };
+  },
+}).mount("#app");
